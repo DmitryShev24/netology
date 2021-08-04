ddf043d8c61d9f8a00144dec9da34bfa9011fcce
Shevchenko Dmitry <dmitriy.e.shevchenko@rt.ru>
2021-08-04 14:40:19 +0300

    вторая домашка

--- game.js
+++ game.js
@@ -1,22 +1,31 @@
-const readline = require("readline");
-const input = readline.createInterface(process.stdin);
-const randomNumber = (min, max) =>  Math.floor(Math.random() * (max - min)) + min
-const range = randomNumber(0, 100)
+const readline = require("readline")
+const input = readline.createInterface(process.stdin)
+const path = require("path")
+const fs = require('fs')
+const yargs = require("yargs/yargs");
+const {hideBin} = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv))
+const file = path.join(__dirname, String(argv.argv._))
 
-console.log('Загадано число от 0 до 100')
+const appendFileLog = (answer) =>{
+    let content = answer
+    fs.appendFile(file, content, (err) => {
+        if (err) throw new Error(err)
+    })
+}
+
+console.log('Введите 1 или 2')
+
 input.on("line", (data) => {
-    newNum = Number(data)
-
-    if (newNum > range ) {
-        console.log('Меньше')
+    let randomNumber = Math.floor(Math.random() * 2 + 1)
+    if ((data != 1) && (data != 2)) {
+        return console.log("Вы ввели отличное от 1 и 2 число!")
     }
-    if (newNum < range ) {
-        console.log('Больше')
+    if (data == randomNumber) {
+        appendFileLog('1,')
+        console.log(`Вы угадали, я и правда загадал ${randomNumber}`)
+    } else {
+        console.log('К сожалению вы не угадали :(')
+        appendFileLog('0,')
     }
-    if (newNum === range) {
-        console.log(`Отгадано число ${range}`)
-        process.exit(0)
-    }
-
 })
-input.on("close", () => console.log('end'));
